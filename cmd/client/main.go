61b1d5381df5919454ced25808036f86dc788bc0
MikeRe85 <45215419+MikeRe85@users.noreply.github.com>
2025-03-02 23:42:49 +0300

    1

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,11 +1,34 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
-*/
 package main
 
-import "github.com/MikeRez0/gophkeeper/cmd/client/cmd"
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/MikeRez0/gophkeeper/cmd/client/cmd"
+)
+
+var buildVersion string
+var buildDate string
+var buildCommit string
+
+const cBuildInfoTemplate = `GophKeeper client
+Build version: %s
+Build date: %s
+Build commit: %s
+OS/Arch: %s/%s
+`
 
 func main() {
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+	fmt.Printf(cBuildInfoTemplate, buildVersion, buildDate, buildCommit, runtime.GOOS, runtime.GOARCH)
+
 	cmd.Execute()
 }
